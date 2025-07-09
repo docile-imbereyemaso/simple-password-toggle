45b4f86e3470a56b2c2ce9e27d0496ab8c4e99be
Docile123 <imbereyemasoo@gmail.com>
2025-07-09 17:57:28 +0200

    finalizing the task

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ togglePassword.addEventListener("click",(event)=>{
     const type = password.getAttribute("type") ==="password" ? "text":"password";
     password.setAttribute("type",type);
      event.target.classList.toggle("bi-eye");
+     
     
 })
 
