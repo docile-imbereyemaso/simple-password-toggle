67a33947a96c5b1dacc7319351f39ccd8c9da8d7
Docile123 <imbereyemasoo@gmail.com>
2025-07-09 17:27:38 +0200

    update the formJ

---
+++ index.js
@@ -0,0 +1,13 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click",(event)=>{
+    const type = password.getAttribute("type") ==="password" ? "text":"password";
+    password.setAttribute("type",type);
+     event.target.classList.toggle("bi-eye");
+    
+})
+
+document.querySelector("form").addEventListener("submit",(event)=>{
+    event.preventDefault();
+});
